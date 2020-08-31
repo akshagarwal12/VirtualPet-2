28910b74c6dfb5d7b332d93f5d64d200deeec527
akshagarwal12 <64213806+akshagarwal12@users.noreply.github.com>
2020-08-31 17:13:29 +0530

    Add files via upload

--- js/food.js
+++ js/food.js
@@ -5,24 +5,40 @@ class Food{
         this.lastFed
     }
     getFoodStock(){
-
+        database.ref("Food").on("value",function(data){
+            foodStock=data.val
+        })
     }
     updateFoodStock(){
-
+        database.ref("/").update({
+            "Food":foodStock
+        })
     }
     deductFood(){
-
+        if(foodStock<=0){
+            foodStock=0
+        }
+        else{
+            foodStock=foodStock-1
+        }
+        database.ref("/").update({
+            "Food":foodStock
+        })
     }
+    
     display(){
-        var x=80, y=100;
-        imageMode(center) 
-        image(this.image,720,220,70,70)
+        var x=90, y=180;
+        imageMode(CENTER) 
+        
 
-        if(this.foodStock!=0){
-            for(var i=0;i<this.foodStock;i++){
+        if(foodStock!=0){
+            for(var i=0;i<foodStock;i++){
                 if(i%10==0){
-                    x=80;
+                    x=90;
+                    y=y+50
                 }
+                image(this.image,x,y,50,50);
+                x=x+30;
             }
         }
 
--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 //Create variables here
-var dog, dog2, database, foodS, foodStock, dog1
+var dog, dog2, database, foodS, foodStock, dog1, lastFed, fedTime
+var addFood,feed,foodObj,renameText,renameButton,dogName
 
 function preload()
 {
@@ -9,31 +10,75 @@ function preload()
 }
 
 function setup() {
-  createCanvas(displayWidth-100,displayHeight-150);
+
+  createCanvas(displayWidth,displayHeight);
+  renameButton=createButton("Rename Dog")
+  renameButton.position(width/2-70,height-(height-100))
+  renameButton.mousePressed(renameDog)
+
+  foodObj=new Food()
+
+  renameText=createInput("Name")
+  renameText.position(width/2-300,height-(height-100))
+
+  feed=createButton("Feed the dog")
+  feed.position(width/2-70,height-(height-80))
+  feed.mousePressed(feedDog)
+
+
+
+
+  addFood=createButton("Add food")
+  addFood.position(width/2+40,height-(height-80))
+  addFood.mousePressed(foodAdd)
+
   database = firebase.database();
   foodStock=database.ref("Food").on("value",readStock)
 
-  dog=createSprite(250,250,900,height-50)
-  scale(-300)
+  dog=createSprite(width-200,height-200,900,height-50)
+  //scale(0.4)
   dog.addImage(dog1)
-  dog.scale=0.5
+  dog.scale=0.4
+  database.ref("Name").on("value",function(data){
+    name=data.val()
+  })
   
 }
 
 
-function draw() {  
-  console.log(foodStock)
+function draw() {
+  database.ref("LastFed").on("value",function(data){
+    lastFed=data.val()
+  })
+
+
+
+  
   background(46, 139, 87)
-  if(keyWentDown(UP_ARROW)){
-    dog.addImage(dog2)
-    writeStock(foodStock);
-  }
+  foodObj.display()
+
+
+
   drawSprites();
 
   //add styles here
   textSize(30)
   fill('black')
-  text(foodStock,250,50)
+  text(foodStock,width/2,height-(height-40))
+  textSize(15)
+  if(lastFed>12){
+    text("Last Time Fed: "+ lastFed%12 +"PM" ,width/2,height/2)
+  }
+  if(lastFed==0){
+    text("Last Time Fed: "+  "12 AM" ,width/2,height/2)
+  }
+  else{
+    text("Last Time Fed: "+  lastFed+" AM" ,width/2,height/2)
+  }
+
+
+  textSize(30)
+  text(name,dog.x,height-400)
 
 }
 function readStock(data){
@@ -52,6 +97,29 @@ function writeStock(x){
   })
 
 }
+function feedDog(){
+  dog.addImage(dog2)
+  writeStock(foodStock);
 
+  database.ref("/").update({
+    "LastFed":hour()
+  })
+}
+function foodAdd(){
+  foodStock++
+  database.ref("/").update({
+    "LastFed":hour()
 
+  })
+  lastFed=hour()
+  database.ref("/").update({
+    "Food":foodStock
+  })
+}
 
+function renameDog(){
+  database.ref("/").update({
+    "Name":renameText.value()
+
+  })
+}
